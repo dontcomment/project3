5a160c87e1f4dfac41dbc21f9eb7fbe83ac6acb9
Roman <romanvoronin1994@gmail.com>
2023-01-15 16:08:11 +0300

    slider

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,17 @@
-alert(1);
+$(document).ready(function(){
+    $('.carousel__inner').slick({
+        speed: 1100,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        prevArrow: '<button type="button" class="slick-prev"><img src="src/icons/left.png"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="src/icons/right.png"></button>',
+        responsive: [
+            {
+              breakpoint: 992,
+              settings: {
+                arrows: false
+              }
+            }
+        ]
+      });
+  });
