c0f9c85047120e70870ba6e597641d707302a62b
Roman <romanvoronin1994@gmail.com>
2023-01-18 18:04:07 +0300

    modal

--- src/js/script.js
+++ src/js/script.js
@@ -14,4 +14,25 @@ $(document).ready(function(){
             }
         ]
       });
+    
+      $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
+      $(this)
+        .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
+        .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
+    });
+    
+    //Modal
+
+    $('[data-modal=consultation]').on('click', function() {
+      $('.overlay, #consultation').fadeIn('slow');
+    });
+
+    $('.modal__close').on('click', function() {
+      $('.overlay, #consultation, #tnx, #order').fadeOut('slow');
+    });
+
+    $('.button_mini').on('click', function() {
+      $('.overlay, #order').fadeIn('slow');
+    });
+
   });
