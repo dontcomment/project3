5dbc981e96a3d2791ab724a5fe67d75c0c21987e
Roman <romanvoronin1994@gmail.com>
2023-01-20 19:29:46 +0300

    last block

--- src/js/script.js
+++ src/js/script.js
@@ -35,4 +35,28 @@ $(document).ready(function(){
       $('.overlay, #order').fadeIn('slow');
     });
 
+    //Smooth scrol
+
+    $(window).scroll(function(){
+      if ($(this).scrollTop() > 1600) {
+        $('.pageup').fadeIn();
+      } else {
+        $('.pageup').fadeOut();
+      }
+    });
+
+    $('.flowing-scroll').on( 'click', function(){ 
+      var el = $(this);
+      var dest = el.attr('href'); // получаем направление
+      if(dest !== undefined && dest !== '') { // проверяем существование
+          $('html').animate({ 
+            scrollTop: $(dest).offset().top // прокручиваем страницу к требуемому элементу
+          }, 500 // скорость прокрутки
+          );
+      }
+      return false;
+  });
+
+
+
   });
